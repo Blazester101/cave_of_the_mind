89c01937d1b125449a6185dee0dc6ab3435a5918
JSchiffb <159210124+Lumpcious@users.noreply.github.com>
2025-03-12 16:03:24 -0600

    Camera Adjustments
    Slight adjustments to camera prefab and some comments to code.

--- Assets/Scripts/CasinoCamera.cs
+++ Assets/Scripts/CasinoCamera.cs
@@ -10,14 +10,14 @@ public class CasinoCamera : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Camera Break");
-        if (other.tag == "Interactable")
+        if (other.tag == "Interactable") //Not necessary in 99% of cases, but keeping if statement here on the off chance player manages to touch the collider themselves. This ensures that it must be an 'interactable' object.
         {
             Rigidbody rg = _casinoCameraBody.GetComponent<Rigidbody>();
-            rg.useGravity = true;
+            rg.useGravity = true; //Causes object to drop from where it was.
 
-            Destroy(_casinoSpotLight);
+            Destroy(_casinoSpotLight); //Light shows where player can't go. Could be fun to let player run around with a giant flashlight, but no.
 
-            Destroy(_casinoOutOfBounds);
+            Destroy(_casinoOutOfBounds); //Destroys out of bounds so player can progress without issue.
 
             
         }
