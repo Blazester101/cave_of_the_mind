f431915d491f39fdb02194dc7b795d3f3eecd772
CryoBlind <cwdean8@gmail.com>
2025-04-08 12:56:22 -0600

    allowed for overriding shiftableshapes shapechanging

--- Assets/Scripts/ShapedObject.cs
+++ Assets/Scripts/ShapedObject.cs
@@ -3,16 +3,21 @@ using UnityEngine;
 public class ShapedObject : MonoBehaviour
 {
     public ObjectShape objectShape;
+    public bool overrideShiftableShape = false;
     private ShiftableObject shiftableComponent;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        if (overrideShiftableShape) return;
+
         shiftableComponent = GetComponent<ShiftableObject>();
         if (shiftableComponent != null) objectShape = shiftableComponent.startingType;
     }
 
     private void Update()
     {
-        if(shiftableComponent != null) objectShape = shiftableComponent.currentType;
+        if (overrideShiftableShape) return;
+
+        if (shiftableComponent != null) objectShape = shiftableComponent.currentType;
     }
 }
